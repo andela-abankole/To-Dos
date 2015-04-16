f52613c029fe9139853b600fdca2d6b65512acce
Akinjide Bankole <jidebankole70@gmail.com>
2015-04-16 17:32:57 +0100

    Add code and few tweaks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,72 @@
 // function to add lists
   function addList(table) {
-      var table = document.getElementById(table);
-        var tableLength = getTableLength(table);
-          var textBoxInput = document.getElementById("textBox").value;
+    var table = document.getElementById(table);
+    var tableLength = getTableLength(table);
+    var textBoxInput = document.getElementById("textBox").value;
 
-  if (textBoxInput === ""){
-        return -1;
-    }
+  if (textBoxInput === ""){return -1;}
+
       var addRow = table.insertRow(0);
 
-// adds list items
-    var addCell = addRow.insertCell(0);
-    var cellValue = document.createElement("input");
-        cellValue.type = "text";
-        cellValue.style.width = "760px";
-        cellValue.style.marginTop = "5px";
-        cellValue.name = "textBox";
-        cellValue.value = textBoxInput;
+        // adds list items
+            var addCell = addRow.insertCell(0);
+            var cellValue = document.createElement("input");
+                cellValue.type = "text";
+                cellValue.style.width = "760px";
+                cellValue.style.marginTop = "5px";
+                cellValue.name = "textBox";
+                cellValue.value = textBoxInput;
 
-// adds check boxes
-    var addCheckBox = addRow.insertCell(1);
-    var checkBoxValue = document.createElement("input");
-        checkBoxValue.type = "checkbox";
-        checkBoxValue.style.width = "15px";
-        checkBoxValue.style.height = "20px";
-        checkBoxValue.style.backgroundColor = "#E3DEDB";
-        checkBoxValue.style.marginTop = "5px";
-        checkBoxValue.name = "checkBox";
+        // adds check boxes
+            var addCheckBox = addRow.insertCell(1);
+            var checkBoxValue = document.createElement("input");
+                checkBoxValue.type = "checkbox";
+                checkBoxValue.style.width = "15px";
+                checkBoxValue.style.height = "20px";
+                checkBoxValue.style.backgroundColor = "#E3DEDB";
+                checkBoxValue.style.marginTop = "5px";
+                checkBoxValue.name = "checkBox";
 
-// adds priority
-    var priority = document.getElementById("selectBox");
-    var priorityValue = priority.options[priority.selectedIndex].value;
-    var priorityText = priority.options[priority.selectedIndex].innerHTML;
-        addRow.id = priorityValue;
+        // adds priority
+            var priority = document.getElementById("selectBox");
+            var priorityValue = priority.options[priority.selectedIndex].value;
+            var priorityText = priority.options[priority.selectedIndex].innerHTML;
+                addRow.id = priorityValue;
 
-    var addPriorityLabel = addRow.insertCell(2);
-    var priorityBoxText = document.createElement("text");
-    var priorityBoxTextNode = document.createTextNode(priorityText);
-        priorityBoxText.appendChild(priorityBoxTextNode);
-        priorityBoxText.style.marginTop = "5px";
-        priorityBoxText.style.color = "white";
-        priorityBoxText.style.fontWeight = "bold";
-        priorityBoxText.name = "textLabel";
+            var addPriorityLabel = addRow.insertCell(2);
+            var priorityBoxText = document.createElement("text");
+            var priorityBoxTextNode = document.createTextNode(priorityText);
+                priorityBoxText.appendChild(priorityBoxTextNode);
+                priorityBoxText.style.marginTop = "5px";
+                priorityBoxText.style.color = "white";
+                priorityBoxText.style.fontWeight = "medium";
+                priorityBoxText.name = "textLabel";
 
-//Sets priority Color for every condition selected
-    if (priorityText === "High") {
-     priorityBoxText.style.backgroundColor = "green";
-    }
-    else if (priorityText === "Medium") {
-      priorityBoxText.style.backgroundColor = "orange";
-    }
-    else if (priorityText === "Low") {
-      priorityBoxText.style.backgroundColor = "red";
-    }
-    else if (priorityText === "None") {
-      priorityBoxText.style.backgroundColor = "transparent";
-    } 
-    else {
-     priorityBoxText.style.backgroundColor = "transparent";
-     priorityBoxText.style.color = "transparent";
-    }
-
-    addCell.appendChild(cellValue);
-    addPriorityLabel.appendChild(priorityBoxText);
-    addCheckBox.appendChild(checkBoxValue);
-    sortList();
-    document.getElementById("textBox").value = "";
+        //Sets priority Color for every condition selected
+      if (priorityText === "High") {
+          priorityBoxText.style.backgroundColor = "green";
+      } else if (priorityText === "Medium") {
+          priorityBoxText.style.backgroundColor = "orange";
+      } else if (priorityText === "Low") {
+          priorityBoxText.style.backgroundColor = "red";
+      }  else if (priorityText === "None") {
+          priorityBoxText.style.backgroundColor = "black";
+      }else {
+          priorityBoxText.style.backgroundColor = "grey";
+          priorityBoxText.style.color = "transparent";
+      }
+            addCell.appendChild(cellValue);
+            addPriorityLabel.appendChild(priorityBoxText);
+            addCheckBox.appendChild(checkBoxValue);
+            sortList();
+            document.getElementById("textBox").value = "";
 }
-
-// returns length of table
-  function getTableLength(rows) {
-      return rows.rows.length;
-  }
-          var date = new Date();
-          document.getElementById("d").innerHTML = date.toUTCString();
+  // returns length of table
+    function getTableLength(rows) {
+        return rows.rows.length;
+    }
+      var date = new Date();
+         document.getElementById("d").innerHTML = date.toUTCString();
 
 // Deletes selected lists
   function deleteList(table) {
